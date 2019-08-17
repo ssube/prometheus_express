d3c144087b590aed6418277ae7af358b7ba9de91
ssube <seansube@gmail.com>
2019-08-17 12:07:17 -0500

    feat: recv data in chunks

--- prometheus_express/http.py
+++ prometheus_express/http.py
@@ -11,6 +11,12 @@ def start_http_server(port, address='0.0.0.0', extraRoutes={}, metricsRoute='/me
     http_socket.bind(bind_address)
     http_socket.listen(1)
 
+    try:
+        http_socket.settimeout(1.0)
+        http_socket.setblocking(0)
+    except OSError:
+        print('Unable to set timeout on socket')
+
     return http_socket
 
 
@@ -18,11 +24,11 @@ def await_http_request(server_socket, registry):
     conn, addr = server_socket.accept()
     print('Connection: {}'.format(addr))
 
-    req = conn.recv(1024).decode(http_encoding)
+    req = read_http_request(conn)
     req_headers = parse_headers(req)
     print('Headers: {}'.format(req_headers))
 
-    if req_headers['path'] == registry.path:
+    if req_headers['method'] == 'GET' and req_headers['path'] == registry.path:
         metrics = registry.print()
         send_http_response(conn, http_break.join(metrics))
     else:
@@ -60,6 +66,7 @@ def print_http_headers(status='200 OK', type='text/plain', length=0):
 
 def parse_headers(req):
     if 'HTTP/' not in req:
+        print('Body ({}): {}'.format(len(req), req))
         raise ValueError('request does not have HTTP/x.y marker')
 
     lines = req.split(http_break)
@@ -70,3 +77,20 @@ def parse_headers(req):
         'path': start[1],
         'http': start[2],
     }
+
+
+def read_http_request(conn):
+    body = bytearray()
+    chunk_max = 256
+
+    while True:
+        print('body: {}'.format(body))
+        chunk = bytearray(b" " * chunk_max)
+
+        bytes = conn.recv_into(chunk, chunk_max)
+        print('chunk read: {}'.format(bytes))
+        if bytes == 0:
+            print('inner body')
+            return body.encode(http_encoding)
+        else:
+            body.extend(chunk)
