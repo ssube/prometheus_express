1bf14b1424a8cc742359d8675eb233bd5fc708b7
ssube <seansube@gmail.com>
2019-12-23 18:38:06 -0600

    feat(scripts): add rudimentary deploy script for lib+example

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+DEVICE_PATH="${1}"
+DEVICE_LIB="${DEVICE_PATH}/lib"
+
+LIBRARY_PATH="${2}"
+
+PROGRAM_FILE="${3}"
+
+mkdir "${DEVICE_LIB}"
+rsync -avh "${LIBRARY_PATH}/neopixel.mpy" "${DEVICE_LIB}"
+rsync -avh --exclude-from="scripts/deploy-exclude" "./prometheus_express" "${DEVICE_LIB}"
+cp -v "./examples/${PROGRAM_FILE}.py" "${DEVICE_PATH}/code.py"
