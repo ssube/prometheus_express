93aa0347fb807938c0801056a8c5066b5bd65f76
ssube <seansube@gmail.com>
2019-12-27 10:00:58 -0600

    fix(scripts): syntax for delay script

--- scripts/bench-wrk.sh
+++ scripts/bench-wrk.sh
@@ -1,9 +1,9 @@
 wrk -c 1 -t 1 -d 60s \
+  http://${EXPRESS_HOST}:8080/metrics \
   --timeout 1s \
-  --script <(echo <<<EOF
+  --script <(cat << EOF
 function delay()
  return math.random(10, 20)
 end
 EOF
-  ) \
-  http://${EXPRESS_HOST}:8080/metrics
+)
