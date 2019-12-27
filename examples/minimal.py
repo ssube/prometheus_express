9623e42cbafe4732c8e9338b2a3858bdb8bf67a5
ssube <seansube@gmail.com>
2019-12-26 20:40:18 -0600

    fix(examples): handle parsing errors in minimal

--- examples/minimal.py
+++ examples/minimal.py
@@ -1,10 +1,5 @@
 # library
 from prometheus_express import check_network, start_http_server, CollectorRegistry, Counter, Gauge, Router
-#from prometheus_express.i2c import scan_i2c_bus
-
-#def check_network(eth):
-#  print('check')
-#  return True
 
 # system
 import board
@@ -26,7 +21,7 @@ BLUE = (0, 0, 255)
 spi = busio.SPI(clock=board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
 eth = wiznet.WIZNET5K(spi, board.D10, board.D11)
-eth.dhcp = True
+server_port = 8080
 
 # initialize the LEDs
 led = digitalio.DigitalInOut(board.D13)
@@ -42,16 +37,15 @@ def main():
 
     registry = CollectorRegistry(namespace='prom_express')
     metric_c = Counter('test_counter',
-                       'a test counter', registry=registry)
+                       'a test counter',
+                       labels=['source'],
+                       registry=registry)
     metric_g = Gauge('test_gauge',
-                     'a test gauge', registry=registry)
+                     'a test gauge',
+                     labels=['source'],
+                     registry=registry)
 
     router = Router([
-        ('GET', '/favicon.ico', lambda headers, body: {
-            'status': '200 OK',
-            'type': 'image/png;base64',
-            'content': 'iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAAFklEQVQI12N8HmPBxMPHxMTDx8TNBwAUNwHSqFS0zAAAAABJRU5ErkJggg==',
-        }),
         ('GET', '/metrics', registry.handler),
     ])
     server = False
@@ -61,19 +55,22 @@ def main():
         ready = check_network(eth)
         led.value = ready
 
-    rgb[0] = BLUE  # connected
-    while not server:
-        server = start_http_server(8080, address=eth.ifconfig()[0])
-
-    rgb[0] = GREEN  # ready
     while True:
-        metric_c.inc(random.randint(0, 50))
-        metric_g.set(random.randint(0, 5000))
+        rgb[0] = BLUE  # connected
+        if not server:
+            server = start_http_server(server_port, address=eth.ifconfig()[0])
+
+        rgb[0] = GREEN  # ready
+        metric_c.labels('heartbeat').inc(1)
+        metric_c.labels('random').inc(random.randint(0, 50))
+        metric_g.labels('clock').set(time.time())
+        metric_g.labels('random').set(random.randint(0, 5000))
         try:
             server.accept(router)
         except OSError as err:
             print('Error accepting request: {}'.format(err))
-            server = start_http_server(8080, address=eth.ifconfig()[0])
+        except ValueError as err:
+            print('Error parsing request: {}'.format(err))
 
 
 main()
