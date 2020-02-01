71bd2fb4777fd5a35ffb00dd6955112c5abd1413
ssube <seansube@gmail.com>
2020-02-01 15:51:07 -0600

    fix(utils): correct temp conversion

--- prometheus_express/utils.py
+++ prometheus_express/utils.py
@@ -31,4 +31,4 @@ def temp_ftoc(temp_f):
     """Convert fahrenheit degrees to celsius.
     Prometheus expects SI units, but some sensors return F.
     """
-    return (temp_f - 32.0) * (9.0 / 5.0)
+    return (temp_f - 32.0) * (5.0 / 9.0)
