1498fb3c325c538e6e790aeb1c0664a84dc4c460
ssube <seansube@gmail.com>
2020-02-02 08:52:54 -0600

    feat(parser): handle comments and empty lines

--- prometheus_express/parser.py
+++ prometheus_express/parser.py
@@ -1,13 +1,4 @@
-def count_indent(line):
-  return len(line) - len(line.lstrip(' '))
-
-def count_next_indent(lines):
-  if len(lines) > 0:
-    return count_indent(lines[0])
-  else:
-    return -1
-
-def parse_file(name):
+def parse_file(name, open=open):
   with open(name, 'r') as source:
     return parse_str(source.readlines())
 
@@ -15,13 +6,17 @@ def parse_str(lines):
   data = {}
   for rline in lines:
     line = rline.strip()
-    if len(line) == 0:
+    if len(line) == 0 or line[0] == '#':
       continue
 
     parts = line.split(':')
     if len(parts) < 2:
       continue
 
-    data[parts[0]] = parts[1].strip()
+    value = ''.join(parts[1:]).strip()
+    if len(value) == 0:
+      continue
+
+    data[parts[0]] = value
 
   return data
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,60 +1,77 @@
 import prometheus_express.utils as pu
 import unittest
 
+
 class MockI2C(object):
-  def __init__(self, locked=False, devices=[]):
-    self.devices = devices
-    self.locked = locked
+    def __init__(self, locked=False, devices=[]):
+        self.devices = devices
+        self.locked = locked
 
-  def try_lock(self):
-    if self.locked:
-      return False
-    else:
-      self.locked = True
-      return True
+    def try_lock(self):
+        if self.locked:
+            return False
+        else:
+            self.locked = True
+            return True
 
-  def scan(self):
-    return self.devices
+    def scan(self):
+        return self.devices
 
-  def unlock(self):
-    self.locked = False
+    def unlock(self):
+        self.locked = False
+
 
 class MockNetwork(object):
-  def __init__(self, connected=False, ip='0.0.0.0'):
-    self.connected = connected
-    self.ip = ip
+    def __init__(self, connected=False, ip='0.0.0.0'):
+        self.connected = connected
+        self.ip = ip
 
-  def ifconfig(self):
-    return [self.ip]
+    def ifconfig(self):
+        return [self.ip]
+
 
 class CheckNetworkTest(unittest.TestCase):
-  def test_unconnected(self):
-    self.assertEqual(
-      pu.check_network(MockNetwork()),
-      False,
-    )
+    def test_unconnected(self):
+        self.assertEqual(
+            pu.check_network(MockNetwork()),
+            False,
+        )
 
-  def test_connected_nullip(self):
-    self.assertEqual(
-      pu.check_network(MockNetwork(connected=True)),
-      False,
-    )
+    def test_connected_nullip(self):
+        self.assertEqual(
+            pu.check_network(MockNetwork(connected=True)),
+            False,
+        )
 
-  def test_connected_withip(self):
-    self.assertEqual(
-      pu.check_network(MockNetwork(connected=True, ip='1.1.1.1')),
-      True,
-    )
+    def test_connected_withip(self):
+        self.assertEqual(
+            pu.check_network(MockNetwork(connected=True, ip='1.1.1.1')),
+            True,
+        )
+
 
 class ScanI2CBusTest(unittest.TestCase):
-  def test_unlocked(self):
-    self.assertEqual(
-      pu.scan_i2c_bus(MockI2C(), 10),
-      True,
-    )
+    def test_unlocked(self):
+        self.assertEqual(
+            pu.scan_i2c_bus(MockI2C(), 10),
+            True,
+        )
 
-  def test_locked(self):
-    self.assertEqual(
-      pu.scan_i2c_bus(MockI2C(locked=True), 10),
-      False,
-    )
+    def test_locked(self):
+        self.assertEqual(
+            pu.scan_i2c_bus(MockI2C(locked=True), 10),
+            False,
+        )
+
+
+class TempFToCTest(unittest.TestCase):
+    def test_convert(self):
+        self.assertAlmostEqual(pu.temp_ftoc(-50), -45.56, 2)
+        self.assertAlmostEqual(pu.temp_ftoc(-40), -40.00, 2)
+        self.assertAlmostEqual(pu.temp_ftoc(-30), -34.44, 2)
+        self.assertAlmostEqual(pu.temp_ftoc(-20), -28.89, 2)
+        self.assertAlmostEqual(pu.temp_ftoc(-10), -23.33, 2)
+        self.assertAlmostEqual(pu.temp_ftoc(0),   -17.78, 2)
+        self.assertAlmostEqual(pu.temp_ftoc(10),  -12.22, 2)
+        self.assertAlmostEqual(pu.temp_ftoc(20),  -6.67,  2)
+        self.assertAlmostEqual(pu.temp_ftoc(32),  0,      2)
